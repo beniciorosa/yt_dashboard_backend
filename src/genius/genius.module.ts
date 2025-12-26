22ff1edc172320291a7650edb5992ab0d4de1104
Dhiego Rosa <dhieeego@gmail.com>
2025-12-26 19:35:10 -0300

    2612

--- src/genius/genius.module.ts
+++ src/genius/genius.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GeniusService } from './genius.service';
 import { GeniusController } from './genius.controller';
+import { OpenaiModule } from '../openai/openai.module';
 
 @Module({
+    imports: [OpenaiModule],
     controllers: [GeniusController],
     providers: [GeniusService],
 })
